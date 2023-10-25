2a47963d9b553a5cbdacd4b590823aaa8cbebcdc
Stepan Sheshukov <sheshukov.s@ya.ru>
2023-10-25 14:17:34 +0500

    Исправил код, но не ушел от ключей 0, хз пока не могу придумать

--- test_api.py
+++ test_api.py
@@ -1,12 +1,15 @@
 """Weather API"""
+
+import api_key
+import api_url
+import api_view
 import requests
 
-api_key = 'adfcc0c9fb5b2a15a8ce28375f9069a4'
+api_key = api_key.set_api()
 
 user_input = input('Введите город: ')
 
-weather_data = requests.get(
-    f'https://api.openweathermap.org/data/2.5/weather?q={user_input}&units=imperial&APPID={api_key}')
+weather_data = requests.get(f'{api_url.set_url(user_input, api_key)}')
 
 if weather_data.json()['cod'] == '404':
     print('Город не найден')
@@ -15,7 +18,4 @@ else:
     temp = round(weather_data.json()['main']['temp'])
     temp_C = round((temp - 32) * 5 / 9)
 
-    # print(weather_data.status_code)
-    # print(weather_data.json())
-    print(f'Температура в {user_input}: {temp} F, {temp_C} C')
-    print(f'Погодные условия: {weather}')
+    print(api_view.view_weather(user_input, temp, temp_C, weather))
